1d8c3f8084a1ca5da98fe836c358dd8d6b6f21a7
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-10-21 23:20:23 +0300

    ХЗ

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-export function checkHealth(object) {
-    let result = object.health > 50 ? "healthy" : "wounded";
+export default function checkHealth(object) {
+  let result = object.health > 50 ? 'healthy' : 'wounded';
 
-    if (object.health < 15) {
-        result = "critical";
-    }
+  if (object.health < 15) {
+    result = 'critical';
+  }
 
-    return result;
+  return result;
 }
